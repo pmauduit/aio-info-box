c08cfffd1e065c129300ad5aaa749418c2e7abeb
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2019-06-18 16:43:05 +0200

    better main.cpp (tests also the OurHome groceries list)

--- main.cpp
+++ main.cpp
@@ -8,8 +8,15 @@
 int main(int argc, char ** argv) {
   OurHome ou;
 
+  std::cout << " BLACK POILUS -- " << std::endl;
+
   ou.login("email", "password");
+
+  std::cout << std::endl << "== Chores (TO-DO)" << std::endl;
   ou.getChores();
+
+  std::cout << std::endl << "== Groceries (shopping list)" << std::endl;
+  ou.getShoppingList();
 
   return 0;
 }
